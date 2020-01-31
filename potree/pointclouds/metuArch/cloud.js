f53cbcd642d8b1fe880f8fc08e1cee1c986ff212
İbrahim SARIÇİÇEK <ibrahim.saricicek@intra.local>
2020-01-31 16:14:08 +0300

    added 3d examples

---
+++ potree/pointclouds/metuArch/cloud.js
@@ -0,0 +1,30 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 892524,
+    "boundingBox": {
+        "lx": 481255.9215,
+        "ly": 4416289.4887,
+        "lz": 874.708,
+        "ux": 481442.8361000007,
+        "uy": 4416476.4033,
+        "uz": 1061.6226000007168
+    },
+    "tightBoundingBox": {
+        "lx": 481255.92,
+        "ly": 4416289.49,
+        "lz": 874.71,
+        "ux": 481405.28,
+        "uy": 4416476.4,
+        "uz": 896.72
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+		"COLOR_PACKED",
+		"NORMAL_SPHEREMAPPED"
+    ],
+    "spacing": 1.6187279224395753,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
