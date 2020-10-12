a9aeb0f8f45c34998fe80bb1f8b329c1f472e4d4
saricicek-pc <ibrahimsaricicek@gmail.com>
2020-10-12 23:21:21 +0300

    haritaankara project

---
+++ haritaankara/precache-manifest.6c667ecce7eb09d65f1cf0b069c54ebb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68483e7c31fdf65fd6d32e7619978c7f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "299c23c0d35be602dc0f",
+    "url": "./static/css/2.39e1324a.chunk.css"
+  },
+  {
+    "revision": "12afbb05a2c07707ee23",
+    "url": "./static/css/main.d2b8a828.chunk.css"
+  },
+  {
+    "revision": "299c23c0d35be602dc0f",
+    "url": "./static/js/2.36d28aa1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.36d28aa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12afbb05a2c07707ee23",
+    "url": "./static/js/main.084d0347.chunk.js"
+  },
+  {
+    "revision": "2b6e02ee8f85d17a6fd7",
+    "url": "./static/js/runtime-main.32adb872.js"
+  }
+]);
