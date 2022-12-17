e19f6e8d6ca162d9b02fb04a2539fc9c81f124ea
isaricicek <isaricicek@kontur.io>
2022-12-18 01:13:56 +0300

    added new layer to haritaankara and change zoom on load

--- haritaankara/service-worker.js
+++ haritaankara/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c3ed169fc151e1ce24ddc07825976cf3.js"
+  "./precache-manifest.0ecc3cac966dcae4979aae199e05b3d1.js"
 );
 
 self.addEventListener('message', (event) => {
